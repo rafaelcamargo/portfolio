e3c89c6ed4fdbc39845e375bed533e855ae5e4d6
Rafael Camargo <hello@rafaelcamargo.com>
2019-09-19 18:34:36 -0300

    chore: add prerender-spa-plugin to production build

--- webpack.conf.prod.js
+++ webpack.conf.prod.js
@@ -2,6 +2,7 @@ const webpack = require('webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
+const PrerenderSPAPlugin = require('prerender-spa-plugin');
 const project = require('./project.json');
 
 module.exports = {
@@ -28,6 +29,17 @@ module.exports = {
     new webpack.SourceMapDevToolPlugin(),
     new MiniCssExtractPlugin({
       filename: project.styles.dist.filename.prod
+    }),
+    new PrerenderSPAPlugin({
+      staticDir: `${__dirname}/${project.scripts.dist.root}`,
+      routes: ['/'],
+      minify: {
+        collapseBooleanAttributes: true,
+        collapseWhitespace: true,
+        decodeEntities: true,
+        keepClosingSlash: true,
+        sortAttributes: true
+      }
     })
   ]
 }
