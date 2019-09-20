ac3275b3aecf9bd6ceaabbd26f181fcb14d2b801
Rafael Camargo <hello@rafaelcamargo.com>
2019-09-19 21:39:45 -0300

    feat: create icon

---
+++ src/scripts/components/icon/icon.js
@@ -0,0 +1,33 @@
+import '@styles/icon.styl';
+import React, { Component } from 'react';
+
+export class RIcon extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      className: buildClassName(props.name),
+      style: buildStyle(props.name)
+    };
+  }
+  render() {
+    return (
+      <div className={ this.state.className } style={ this.state.style }></div>
+    );
+  }
+}
+
+function buildClassName(name){
+  return `r-icon r-icon-${name}`;
+}
+
+function buildStyle(name){
+  const maskImageValue = buildMaskImageValue(name);
+  return {
+    maskImage: maskImageValue,
+    WebkitMaskImage: maskImageValue
+  };
+}
+
+function buildMaskImageValue(name){
+  return `url("images/icon-${name}.svg")`;
+}
