9ea0d28579f5ca500fdd97e3560449e8c12bd725
Rafael Camargo <hello@rafaelcamargo.com>
2019-09-21 14:02:11 -0300

    chore: add feature list and recommendation list to home
    also, hide topbar menu on mobile

--- src/scripts/home/home.js
+++ src/scripts/home/home.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 import { RCol } from '@scripts/base/components/col/col';
 import { RCtaLink } from '@scripts/base/components/cta-link/cta-link';
+import { RFeature } from '@scripts/base/components/feature/feature';
+import { RFeatureList } from '@scripts/base/components/feature-list/feature-list';
 import { RHero } from '@scripts/base/components/hero/hero';
 import { RRecommendation } from '@scripts/recommendations/components/recommendation/recommendation';
+import { RRecommendationList } from '@scripts/recommendations/components/recommendation-list/recommendation-list';
 import { RRow } from '@scripts/base/components/row/row';
 import { RSection } from '@scripts/base/components/section/section';
 import { RViewport } from '@scripts/base/components/viewport/viewport';
@@ -40,11 +43,11 @@ export class Home extends Component {
         </RSection>
         <RSection theme="primary">
           <RRow>
-            <RCol size="6">
-              <RRecommendation id="matias" />
-            </RCol>
-            <RCol size="6">
-              <RRecommendation id="bruckheimer" />
+            <RCol size="12">
+              <RRecommendationList>
+                <RRecommendation id="matias" />
+                <RRecommendation id="bruckheimer" />
+              </RRecommendationList>
             </RCol>
           </RRow>
           <RRow align="center" offset="10">
@@ -55,74 +58,39 @@ export class Home extends Component {
         </RSection>
         <RSection>
           <RRow>
-            <RCol size="4">
-              <RRow>
-                <RCol size="12">
-                  <h2>
-                    Few time. Lots of impact.
-                  </h2>
-                </RCol>
-              </RRow>
-              <RRow>
-                <RCol size="12">
+            <RCol size="12">
+              <RFeatureList itemSize="1-3">
+                <RFeature
+                  title="Few time. Lots of impact."
+                  ctaTo="/experience"
+                  ctaText="See my experience">
                   <p>
                     I started programming in 2013, not so long.<br />
                     But on this short period of time, I collaborated in activities
                     that generated a huge impact.
                   </p>
-                </RCol>
-              </RRow>
-              <RRow>
-                <RCol size="12">
-                  <RCtaLink to="/experience" text="See my experience" />
-                </RCol>
-              </RRow>
-            </RCol>
-            <RCol size="4">
-              <RRow>
-                <RCol size="12">
-                  <h2>
-                    Building is the way I best learn.
-                  </h2>
-                </RCol>
-              </RRow>
-              <RRow>
-                <RCol size="12">
+                </RFeature>
+                <RFeature
+                  title="Building is the way I best learn."
+                  ctaTo="/projects"
+                  ctaText="See my projects">
                   <p>
                     I am always full of new ideias.<br />
                     Building a side project is the way
                     I best learn something new.
                   </p>
-                </RCol>
-              </RRow>
-              <RRow>
-                <RCol size="12">
-                  <RCtaLink to="/projects" text="See my projects" />
-                </RCol>
-              </RRow>
-            </RCol>
-            <RCol size="4">
-              <RRow>
-                <RCol size="12">
-                  <h2>
-                    A couple of stories to share.
-                  </h2>
-                </RCol>
-              </RRow>
-              <RRow>
-                <RCol size="12">
+                </RFeature>
+                <RFeature
+                  title="A couple of stories to share."
+                  ctaTo="/stories"
+                  ctaText="See my stories">
                   <p>
                     Writing is a craft that I'm striving to learn.<br />
                     Once in a while, I dedicate some time to share a bit of the
                     experience I'm getting along the way.
                   </p>
-                </RCol>
-              </RRow>
-              <RRow>
-                <RCol size="12">
-                  <RCtaLink to="/stories" text="See my stories" />
-                </RCol>
-              </RRow>
+                </RFeature>
+              </RFeatureList>
             </RCol>
           </RRow>
         </RSection>
