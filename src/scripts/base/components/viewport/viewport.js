6b4783d4433311e835b8ea4f66faba3f0c2545a0
Rafael Camargo <hello@rafaelcamargo.com>
2019-09-21 16:02:26 -0300

    chore: add sidebar to viewport

--- src/scripts/base/components/viewport/viewport.js
+++ src/scripts/base/components/viewport/viewport.js
@@ -4,12 +4,14 @@ import { RCol } from '@scripts/base/components/col/col';
 import { RContainer } from '@scripts/base/components/container/container';
 import { RFooter } from '@scripts/base/components/footer/footer';
 import { RRow } from '@scripts/base/components/row/row';
+import { RSidebar } from '@scripts/base/components/sidebar/sidebar';
 import { RTopbar } from '@scripts/base/components/topbar/topbar';
 
 export class RViewport extends Component {
   render() {
     return (
       <main className="r-viewport">
+        <RSidebar />
         <RTopbar />
         <article className="r-viewport-content">
           { this.props.children }
