9920ae7bf8990941a4a9d0550b45218fa7a43ca6
Rafael Camargo <hello@rafaelcamargo.com>
2019-09-22 14:33:01 -0300

    chore: improve router

--- src/scripts/router.js
+++ src/scripts/router.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ScrollToTop from 'react-router-scroll-top';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Experience } from '@scripts/experience/experience';
 import { Home } from '@scripts/home/home';
 import { Skills } from '@scripts/skills/skills';
-import { Experience } from '@scripts/experience/experience';
 
 export function AppRouter() {
   return (
     <Router>
       <ScrollToTop>
         <Route path="/" exact component={ Home } />
-        <Route path="/skills" exact component={ Skills } />
         <Route path="/experience" exact component={ Experience } />
+        <Route path="/skills" exact component={ Skills } />
       </ScrollToTop>
     </Router>
   );
--- src/scripts/router.test.js
+++ src/scripts/router.test.js
@@ -1,20 +1,43 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { Home } from '@scripts/home/home';
+import ScrollToTop from 'react-router-scroll-top';
 import { AppRouter } from './router';
+import { Experience } from '@scripts/experience/experience';
+import { Home } from '@scripts/home/home';
+import { Skills } from '@scripts/skills/skills';
 
 describe('Home', () => {
+  function mount(){
+    return shallow(<AppRouter />);
+  }
+
   it('should contain a Router', () => {
-    const wrapper = shallow(<AppRouter />);
+    const wrapper = mount();
     expect(wrapper.find(Router)).toBeDefined();
   });
 
   it('should contain Home route', () => {
-    const wrapper = shallow(<AppRouter />);
-    const route = wrapper.find(Router).children().at(0);
+    const wrapper = mount();
+    const route = wrapper.find(ScrollToTop).children().at(0);
     expect(route.prop('path')).toEqual('/');
     expect(route.prop('exact')).toEqual(true);
     expect(route.prop('component')).toEqual(Home);
+  });
+
+  it('should contain Experience route', () => {
+    const wrapper = mount();
+    const route = wrapper.find(ScrollToTop).children().at(1);
+    expect(route.prop('path')).toEqual('/experience');
+    expect(route.prop('exact')).toEqual(true);
+    expect(route.prop('component')).toEqual(Experience);
+  });
+
+  it('should contain Skills route', () => {
+    const wrapper = mount();
+    const route = wrapper.find(ScrollToTop).children().at(2);
+    expect(route.prop('path')).toEqual('/skills');
+    expect(route.prop('exact')).toEqual(true);
+    expect(route.prop('component')).toEqual(Skills);
   });
 });
