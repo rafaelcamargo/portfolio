b2c6aa6a90984e29a6488b7e51afacdbbc11cc22
Rafael Camargo <hello@rafaelcamargo.com>
2019-09-22 01:41:42 -0300

    chore: make window scroll to top on route change

--- src/scripts/router.js
+++ src/scripts/router.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ScrollToTop from 'react-router-scroll-top';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Home } from '@scripts/home/home';
 import { Skills } from '@scripts/skills/skills';
@@ -7,9 +8,11 @@ import { Experience } from '@scripts/experience/experience';
 export function AppRouter() {
   return (
     <Router>
-      <Route path="/" exact component={ Home } />
-      <Route path="/skills" exact component={ Skills } />
-      <Route path="/experience" exact component={ Experience } />
+      <ScrollToTop>
+        <Route path="/" exact component={ Home } />
+        <Route path="/skills" exact component={ Skills } />
+        <Route path="/experience" exact component={ Experience } />
+      </ScrollToTop>
     </Router>
   );
 }
