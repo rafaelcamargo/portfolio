dc401bd554fb11b771db6652fec54da397dd2916
Rafael Camargo <hello@rafaelcamargo.com>
2019-09-23 02:47:44 -0300

    chore: add meta to viewport

--- src/scripts/base/components/viewport/viewport.js
+++ src/scripts/base/components/viewport/viewport.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import { RCol } from '@scripts/base/components/col/col';
 import { RContainer } from '@scripts/base/components/container/container';
 import { RFooter } from '@scripts/base/components/footer/footer';
+import { RMeta } from '@scripts/base/components/meta/meta';
 import { RRow } from '@scripts/base/components/row/row';
 import { RSidebar } from '@scripts/base/components/sidebar/sidebar';
 import { RTopbar } from '@scripts/base/components/topbar/topbar';
@@ -10,20 +11,30 @@ import { RTopbar } from '@scripts/base/components/topbar/topbar';
 export class RViewport extends Component {
   render() {
     return (
-      <main className="r-viewport">
-        <RSidebar />
-        <RTopbar />
-        <article className="r-viewport-content">
-          { this.props.children }
-        </article>
-        <RContainer>
-          <RRow>
-            <RCol size="12">
-              <RFooter />
-            </RCol>
-          </RRow>
-        </RContainer>
-      </main>
+      <div className="r-viewport">
+        <RMeta
+          title={ this.props.title }
+          description={ this.props.description }
+          keywords={ this.props.keywords }
+          twitterCard={ this.props.twitterCard }
+          image={ this.props.image }/>
+        <header>
+          <RSidebar />
+          <RTopbar />
+        </header>
+        <main>
+          <article className="r-viewport-content">
+            { this.props.children }
+          </article>
+          <RContainer>
+            <RRow>
+              <RCol size="12">
+                <RFooter />
+              </RCol>
+            </RRow>
+          </RContainer>
+        </main>
+      </div>
     );
   }
 }
--- src/scripts/base/components/viewport/viewport.test.js
+++ src/scripts/base/components/viewport/viewport.test.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import { RMeta } from '@scripts/base/components/meta/meta';
 import { RViewport } from '@scripts/base/components/viewport/viewport';
 
 describe('Row', () => {
   function mount(props = {}){
     return shallow(
-      <RViewport>
+      <RViewport
+        title={ props.title }
+        description={ props.description }
+        keywords={ props.keywords }
+        twitterCard={ props.twitterCard }
+        image={ props.image }>
         { props.content }
       </RViewport>
     );
@@ -20,5 +26,19 @@ describe('Row', () => {
     const content = <p>Hello!</p>;
     const wrapper = mount({ content });
     expect(wrapper.find('p').text().trim()).toEqual('Hello!');
+  });
+
+  it('should accept meta info', () => {
+    const title = 'Title';
+    const description = 'Description';
+    const keywords = 'key, words';
+    const twitterCard = 'summary_large_image';
+    const image = 'portfolio.png';
+    const wrapper = mount({ title, description, keywords, twitterCard, image });
+    expect(wrapper.find(RMeta).prop('title')).toEqual(title);
+    expect(wrapper.find(RMeta).prop('description')).toEqual(description);
+    expect(wrapper.find(RMeta).prop('keywords')).toEqual(keywords);
+    expect(wrapper.find(RMeta).prop('twitterCard')).toEqual(twitterCard);
+    expect(wrapper.find(RMeta).prop('image')).toEqual(image);
   });
 });
