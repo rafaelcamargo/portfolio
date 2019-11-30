b4f3be7b8a39b9698ad89c45b8f05b6e9ddf5594
Rafael Camargo <hello@rafaelcamargo.com>
2019-11-30 02:26:45 -0300

    feat: add 'component maturity model' to story list

--- src/scripts/stories/components/story-list/story-list.js
+++ src/scripts/stories/components/story-list/story-list.js
@@ -1,6 +1,5 @@
 import '@styles/story-list.styl';
 import React, { Component } from 'react';
-import { RCtaLink } from '@scripts/base/components/cta-link/cta-link';
 import storiesService from '@scripts/stories/services/stories/stories';
 import imagesService from '@scripts/base/services/images/images';
 
@@ -23,12 +22,21 @@ function buildItem(story){
           <div className="r-story-list-header">
             <img src={ buildImageUrl(story.image.filename) } alt={ story.image.alt } />
           </div>
-          <h2>{ story.title }</h2>
+          <h2><a href={ story.url } target="_blank">{ story.title }</a></h2>
           <p>{ story.description }</p>
-          <RCtaLink text="Read full story" href={ story.url } />
+          { buildPortugueseLink(story) }
         </li>;
 }
 
 function buildImageUrl(filename){
   return `${ imagesService.getBasePath() }/${filename}`;
+}
+
+function buildPortugueseLink({ pt }){
+  return pt ?
+    <div className="r-story-list-portuguese">
+      <h3>Leia em Protuguês</h3>
+      <a href={ pt.url } target="_blank" data-story-list-pt-link>{ pt.title }</a>
+    </div> :
+    null;
 }
--- src/scripts/stories/mocks/stories.js
+++ src/scripts/stories/mocks/stories.js
@@ -6,6 +6,18 @@ const stories = [{
     filename: "story.svg",
     alt: "story illustration"
   }
+}, {
+  title: "Title 2",
+  description: "Description 2",
+  url: "https://some.story.com/2",
+  image: {
+    filename: "story-2.svg",
+    alt: "story illustration 2"
+  },
+  pt: {
+    title: "Title PT",
+    url: "https://some.story.com/pt"
+  }
 }];
 
 export default stories;
