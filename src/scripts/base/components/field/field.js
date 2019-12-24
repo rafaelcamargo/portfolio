d7ff755c0262f4eb2a4e411ecc85d14c7150f5ad
Rafael Camargo <hello@rafaelcamargo.com>
2019-12-24 16:38:36 -0300

    feat: create field component

---
+++ src/scripts/base/components/field/field.js
@@ -0,0 +1,23 @@
+import '@styles/field.styl';
+import React, { Component } from 'react';
+
+export class RField extends Component {
+  render() {
+    return (
+      <div className="r-field">
+        <div className="r-field-label-wrapper">
+          <label className={ buildLabelClassName(this.props.children) }>
+            { this.props.label }
+          </label>
+        </div>
+        <div className="r-field-content">
+          { this.props.children }
+        </div>
+      </div>
+    );
+  }
+}
+
+function buildLabelClassName(children){
+  return children && children.props.required ? 'r-field-required' : '';
+}
