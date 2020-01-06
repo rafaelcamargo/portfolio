7b3afbc97c96a5ff3bf87e281d6a1beb8ca0aca0
Rafael Camargo <hello@rafaelcamargo.com>
2020-01-05 22:09:32 -0300

    feat: make share component multi language

--- src/scripts/base/components/share/share.test.js
+++ src/scripts/base/components/share/share.test.js
@@ -7,12 +7,16 @@ import analyticsService from '@scripts/base/services/analytics/analytics';
 describe('Share', () => {
   function mount(props = {}){
     return shallow(
-      <RShare message={ props.message } url={ props.url } />
+      <RShare message={ props.message } url={ props.url } lang={ props.lang } />
     );
   }
 
   function getExpectedTweetPrefix(){
     return 'Check out this story written by @rcamargo:';
+  }
+
+  function getLabelText(wrapper){
+    return wrapper.find('[data-share-label]').text();
   }
 
   beforeEach(() => {
@@ -25,7 +29,17 @@ describe('Share', () => {
     expect(wrapper.prop('className')).toEqual('r-share');
   });
 
-  it('should open twitter sharing window on twitter button click', () => {
+  it('should show a label in english by default', () => {
+    const wrapper = mount();
+    expect(getLabelText(wrapper)).toEqual('Share');
+  });
+
+  it('should optionally show a label in portuguese', () => {
+    const wrapper = mount({ lang: 'pt' });
+    expect(getLabelText(wrapper)).toEqual('Compartilhe');
+  });
+
+  it('should share a tweet in english by default on twitter button click', () => {
     const message = 'Just discover an amazing content!';
     const url = 'https://rafaelcamargo.com';
     const wrapper = mount({ message, url });
@@ -33,6 +47,17 @@ describe('Share', () => {
     expect(routeService.openUrl).toHaveBeenCalledWith(
       'https://twitter.com/intent/tweet',
       { text: `${getExpectedTweetPrefix()} "${message}" ${url}` }
+    );
+  });
+
+  it('should optionally share a tweet in portuguese on twitter button click', () => {
+    const message = 'Descubra um conteúdo incrível';
+    const url = 'https://rafaelcamargo.com';
+    const wrapper = mount({ message, url, lang: 'pt' });
+    wrapper.find('[data-share-twitter-button]').simulate('click');
+    expect(routeService.openUrl).toHaveBeenCalledWith(
+      'https://twitter.com/intent/tweet',
+      { text: `Confere aí esse texto escrito pelo @rcamargo: "${message}" ${url}` }
     );
   });
 
--- src/scripts/stories/components/story-viewport/story-viewport.test.js
+++ src/scripts/stories/components/story-viewport/story-viewport.test.js
@@ -20,6 +20,11 @@ describe('Story Viewport', () => {
           title: 'title',
           description: 'description',
           keywords: 'keywords'
+        },
+        pt: {
+          title: 'título',
+          description: 'descrição',
+          keywords: 'palavras-chave'
         }
       }
     }
@@ -28,8 +33,8 @@ describe('Story Viewport', () => {
   function mount(props = {}){
     return shallow(
       <RStoryViewport
-        storySummaryId={props.storySummaryId}
-        primaryLanguage="en">
+        storySummaryId={ props.storySummaryId }
+        primaryLanguage={ props.primaryLanguage }>
         { props.content }
       </RStoryViewport>
     );
@@ -41,12 +46,12 @@ describe('Story Viewport', () => {
   });
 
   it('should have appropriate css class', () => {
-    const wrapper = mount({ storySummaryId: 1 });
+    const wrapper = mount({ storySummaryId: 1, primaryLanguage: 'en' });
     expect(wrapper.prop('className')).toEqual('r-story-viewport');
   });
 
   it('should build a viewport with story summary', () => {
-    const wrapper = mount({ storySummaryId: 1 });
+    const wrapper = mount({ storySummaryId: 1, primaryLanguage: 'en' });
     expect(wrapper.find(RViewport).prop('title')).toEqual('title');
     expect(wrapper.find(RViewport).prop('description')).toEqual('description');
     expect(wrapper.find(RViewport).prop('keywords')).toEqual('keywords');
@@ -54,27 +59,28 @@ describe('Story Viewport', () => {
   });
 
   it('should build a hero with story summary title', () => {
-    const wrapper = mount({ storySummaryId: 1 });
+    const wrapper = mount({ storySummaryId: 1, primaryLanguage: 'en' });
     expect(wrapper.find(RHero).prop('title')).toEqual('title');
     expect(wrapper.find(RHero).prop('size')).toEqual('small');
   });
 
   it('should build an image', () => {
-    const wrapper = mount({ storySummaryId: 1 });
+    const wrapper = mount({ storySummaryId: 1, primaryLanguage: 'en' });
     expect(wrapper.find(RImage).prop('filename')).toEqual('components.svg');
     expect(wrapper.find(RImage).prop('alt')).toEqual('components');
   });
 
   it('should contain a share component', () => {
-    const wrapper = mount({ storySummaryId: 1 });
+    const wrapper = mount({ storySummaryId: 1, primaryLanguage: 'pt' });
     const share = wrapper.find(RShare).at(0);
-    expect(share.prop('message')).toEqual('title');
+    expect(share.prop('message')).toEqual('título');
     expect(share.prop('url')).toEqual('http://some.url.com');
+    expect(share.prop('lang')).toEqual('pt');
   });
 
   it('should transclude some content', () => {
     const content = <b>Hey</b>;
-    const wrapper = mount({ storySummaryId: 1, content });
+    const wrapper = mount({ storySummaryId: 1, primaryLanguage: 'en', content });
     expect(wrapper.find('b').text()).toEqual('Hey');
   });
 });
