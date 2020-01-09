24899420caf01d4e0b548087a2a35d174e908aae
Rafael Camargo <hello@rafaelcamargo.com>
2020-01-09 00:02:01 -0300

    feat: create divider component

---
+++ src/scripts/base/components/divider/divider.js
@@ -0,0 +1,10 @@
+import '@styles/divider.styl';
+import React, { Component } from 'react';
+
+export class RDivider extends Component {
+  render() {
+    return (
+      <div className="r-divider"></div>
+    );
+  }
+}
