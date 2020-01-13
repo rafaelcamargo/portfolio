c6e93b02a815b310a81fc9f49625c1f023fb4ea0
Rafael Camargo <hello@rafaelcamargo.com>
2020-01-13 20:30:31 -0300

    feat: add cta sentence to home hero

--- src/scripts/home/components/home-hero/home-hero.js
+++ src/scripts/home/components/home-hero/home-hero.js
@@ -1,5 +1,6 @@
 import '@styles/home-hero.styl';
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { RHero } from '@scripts/base/components/hero/hero';
 import { RHomeMobilePanel } from '@scripts/home/components/home-mobile-panel/home-mobile-panel';
 
@@ -8,9 +9,15 @@ export class RHomeHero extends Component {
     return (
       <div className="r-home-hero">
         <RHero size="large">
-          <h1>
-            My name is Rafael.<br />I am an expert in building UI components.
-          </h1>
+          <div className="r-home-hero-copy-container">
+            <h1>
+              My name is Rafael.<br />I am an expert in building UI components.
+            </h1>
+            <p>
+              I am always looking for great projects. Got a good one? <Link to="/contact">
+              Contact me!</Link>
+            </p>
+          </div>
           <div className="r-home-hero-art-container">
             <RHomeMobilePanel />
           </div>
