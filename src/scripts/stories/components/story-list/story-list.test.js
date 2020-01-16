bd43d62ea969d320c3ebb075051eb08dd3ad6559
Rafael Camargo <hello@rafaelcamargo.com>
2020-01-15 23:51:26 -0300

    chore: improve story list
    added summaryIds, primaryLanguage and secondaryLanguage props

--- src/scripts/stories/components/story-list/story-list.test.js
+++ src/scripts/stories/components/story-list/story-list.test.js
@@ -8,7 +8,10 @@ import storiesService from '@scripts/stories/services/stories/stories';
 describe('Story List', () => {
   function mount(props = {}){
     return shallow(
-      <RStoryList />
+      <RStoryList
+        summaryIds={ props.summaryIds }
+        primaryLanguage={ props.primaryLanguage }
+        secondaryLanguage={ props.secondaryLanguage } />
     );
   }
 
@@ -21,10 +24,30 @@ describe('Story List', () => {
     expect(wrapper.prop('className')).toEqual('r-story-list');
   });
 
+  it('should accept a primary language as prop', () => {
+    const wrapper = mount({ primaryLanguage: 'pt' });
+    const summaryItems = wrapper.find(RStorySummary);
+    expect(summaryItems.at(0).prop('primaryLanguage')).toEqual('pt');
+  });
+
+  it('should accept a secondary language as prop', () => {
+    const wrapper = mount({ secondaryLanguage: 'en' });
+    const summaryItems = wrapper.find(RStorySummary);
+    expect(summaryItems.at(0).prop('secondaryLanguage')).toEqual('en');
+  });
+
   it('should reverse items order', () => {
     const wrapper = mount();
     const summaryItems = wrapper.find(RStorySummary);
     const lastSummary = summaryItems.at(summaryItems.length - 1).prop('summary');
     expect(lastSummary.id).toEqual(1);
+  });
+
+  it('should optionally filter summaries', () => {
+    const wrapper = mount({ summaryIds: [1,3] });
+    const summaryItems = wrapper.find(RStorySummary);
+    expect(summaryItems.length).toEqual(2);
+    expect(summaryItems.at(0).prop('summary').id).toEqual(3);
+    expect(summaryItems.at(1).prop('summary').id).toEqual(1);
   });
 });
