8a6d97c610af58561d9eaac000a99b35284b392c
Rafael Camargo <hello@rafaelcamargo.com>
2020-05-09 18:59:57 -0300

    feat: improve hero link
    add prop to

--- src/scripts/base/components/hero-link/hero-link.js
+++ src/scripts/base/components/hero-link/hero-link.js
@@ -1,5 +1,6 @@
 import '@styles/hero-link.styl';
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export class RHeroLink extends Component {
   onClick = evt => {
@@ -10,10 +11,20 @@ export class RHeroLink extends Component {
   render() {
     return (
       <span className="r-hero-link">
-        <a href={ this.props.href } onClick={ this.onClick } target="_blank">
-          { this.props.text }
-        </a>
+        { buildLink(this.props, this.onClick) }
       </span>
     );
   }
+}
+
+function buildLink({ to, href, text }, onClick){
+  return to ? buildRouterLink(to, text): buildAnchor(href, text, onClick);
+}
+
+function buildAnchor(href, text, onClick){
+  return <a href={ href } onClick={ onClick } target="_blank">{ text }</a>;
+}
+
+function buildRouterLink(to, text){
+  return <Link to={ to }>{ text }</Link>
 }
--- src/scripts/base/components/hero-link/hero-link.test.js
+++ src/scripts/base/components/hero-link/hero-link.test.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import { Link } from 'react-router-dom';
 import { RHeroLink } from './hero-link';
 
 describe('Hero Link', () => {
   function mount(props = {}){
     return shallow(
       <RHeroLink
+        to={ props.to }
         href={ props.href }
         text={ props.text }
         onClick={ props.onClick } />
@@ -23,6 +25,14 @@ describe('Hero Link', () => {
     const wrapper = mount({ href, text });
     expect(wrapper.find('a').prop('href')).toEqual(href);
     expect(wrapper.find('a').text()).toEqual(text);
+  });
+
+  it('should optionally render a router link', () => {
+    const to = '/contact';
+    const text = 'Contact';
+    const wrapper = mount({ to, text });
+    expect(wrapper.find(Link).prop('to')).toEqual(to);
+    expect(wrapper.find(Link).prop('children')).toEqual(text);
   });
 
   it('should execute click callback if callback has been passed', () => {
