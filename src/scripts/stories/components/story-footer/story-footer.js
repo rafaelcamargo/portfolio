76bff092c7f82c94ddabaa442f7c126fc1581ec8
Rafael Camargo <hello@rafaelcamargo.com>
2020-05-24 19:00:54 -0300

    feat: add pitsby promo to story footer component

--- src/scripts/stories/components/story-footer/story-footer.js
+++ src/scripts/stories/components/story-footer/story-footer.js
@@ -1,25 +1,12 @@
 import '@styles/story-footer.styl';
 import React, { Component } from 'react';
-import { RDivider } from '@scripts/base/components/divider/divider';
-import { RNewsletter } from '@scripts/base/components/newsletter/newsletter';
-import { RShare } from '@scripts/base/components/share/share';
+import { RPitsbyPromo } from '@scripts/base/components/pitsby-promo/pitsby-promo';
 
 export class RStoryFooter extends Component {
   render() {
     return (
-      <footer className="r-story-footer" lang={ this.props.lang }>
-        <RDivider />
-        <div className="r-story-footer-content">
-          <div className="r-story-footer-newsletter">
-            <RNewsletter lang={ this.props.lang } />
-          </div>
-          <div className="r-story-footer-share">
-            <RShare
-              message={ this.props.shareMessage }
-              url={ this.props.shareUrl }
-              lang={ this.props.lang } />
-          </div>
-        </div>
+      <footer className="r-story-footer">
+        <RPitsbyPromo />
       </footer>
     );
   }
--- src/scripts/stories/components/story-footer/story-footer.test.js
+++ src/scripts/stories/components/story-footer/story-footer.test.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { RDivider } from '@scripts/base/components/divider/divider';
-import { RNewsletter } from '@scripts/base/components/newsletter/newsletter';
-import { RShare } from '@scripts/base/components/share/share';
+import { RPitsbyPromo } from '@scripts/base/components/pitsby-promo/pitsby-promo';
 import { RStoryFooter } from './story-footer';
 
-describe('Bottles and Principles EN', () => {
-  function mount(props = {}){
+describe('Story Footer', () => {
+  function mount(){
     return shallow(
-      <RStoryFooter
-        shareMessage={ props.shareMessage }
-        shareUrl={ props.shareUrl }
-        lang={ props.lang } />
+      <RStoryFooter />
     );
   }
 
@@ -20,32 +15,8 @@ describe('Bottles and Principles EN', () => {
     expect(wrapper.prop('className')).toEqual('r-story-footer');
   });
 
-  it('should accept a language', () => {
-    const lang = 'pt';
-    const wrapper = mount({ lang });
-    expect(wrapper.find(RNewsletter).prop('lang')).toEqual(lang);
-    expect(wrapper.find(RShare).prop('lang')).toEqual(lang);
-  });
-
-  it('should accept a sharing message', () => {
-    const shareMessage = 'message';
-    const wrapper = mount({ shareMessage });
-    expect(wrapper.find(RShare).prop('message')).toEqual(shareMessage);
-  });
-
-  it('should accept a sharing url', () => {
-    const shareUrl = 'http://some.url.com';
-    const wrapper = mount({ shareUrl });
-    expect(wrapper.find(RShare).prop('url')).toEqual(shareUrl);
-  });
-
-  it('should contain a divider component', () => {
+  it('should contain Pitsby promo component', () => {
     const wrapper = mount();
-    expect(wrapper.find(RDivider).length).toEqual(1);
-  });
-
-  it('should contain a newsletter component', () => {
-    const wrapper = mount();
-    expect(wrapper.find(RNewsletter).length).toEqual(1);
+    expect(wrapper.find(RPitsbyPromo).length).toEqual(1);
   });
 });
