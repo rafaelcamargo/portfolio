920e7adb79f26088be6163e3119872442174928c
Rafael Camargo <hello@rafaelcamargo.com>
2020-06-14 13:17:21 -0300

    feat: improve story footer
    add newsletter modal trigger

---
+++ src/scripts/stories/components/story-footer/story-footer-locales.js
@@ -0,0 +1,10 @@
+export default {
+  en: {
+    newsletter: 'Once a month, you get an email with the latest stories.',
+    newsletterLink: 'Subscribe'
+  },
+  pt: {
+    newsletter: 'Todo mês, um email com os últimos textos publicados.',
+    newsletterLink: 'Inscreva-se'
+  }
+}
--- src/scripts/stories/components/story-footer/story-footer.js
+++ src/scripts/stories/components/story-footer/story-footer.js
@@ -1,14 +1,31 @@
 import '@styles/story-footer.styl';
 import React, { Component } from 'react';
 import { RDivider } from '@scripts/base/components/divider/divider';
+import { RNewsletterModalTrigger } from '@scripts/newsletter/components/newsletter-modal-trigger/newsletter-modal-trigger';
 import { RPitsbyPromo } from '@scripts/base/components/pitsby-promo/pitsby-promo';
 import { RStoryRelations } from '@scripts/stories/components/story-relations/story-relations';
+import locales from './story-footer-locales';
 
 export class RStoryFooter extends Component {
+  constructor(props){
+    super(props);
+    const lang = props.lang || 'en';
+    this.setTexts(locales[lang]);
+  }
+
+  setTexts(texts){
+    this.texts = texts;
+  }
+
   render() {
     return (
       <footer className="r-story-footer">
         { buildStoryRelation(this.props.storyRelationIds, this.props.lang) }
+        <p className="r-story-footer-newsletter-statement">
+          { this.texts.newsletter } <RNewsletterModalTrigger lang={ this.props.lang }>
+            { this.texts.newsletterLink }
+          </RNewsletterModalTrigger>.
+        </p>
         <RDivider />
         <RPitsbyPromo />
       </footer>
