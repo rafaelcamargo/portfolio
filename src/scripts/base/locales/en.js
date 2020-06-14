aa42dc4a1bb64b4cae9368ee22c9725f163b0abe
Rafael Camargo <hello@rafaelcamargo.com>
2020-06-13 21:22:49 -0300

    chore: remove newsletter component

--- src/scripts/base/locales/en.js
+++ src/scripts/base/locales/en.js
@@ -2,9 +2,5 @@ export default {
   Share: {
     tweetPrefix: 'Check out this story written by @rcamargo:',
     label: 'Share'
-  },
-  Newsletter: {
-    cta: 'Don\'t miss a thing, subscribe to my newsletter!',
-    link: 'Subscribe'
   }
 }
