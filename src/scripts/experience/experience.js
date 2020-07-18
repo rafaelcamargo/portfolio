5b62080c367b32075dfd00d86c722284a2fc9c2c
Rafael Camargo <hello@rafaelcamargo.com>
2020-07-18 14:06:04 -0300

    feat: improve experience copy

--- src/scripts/experience/experience.js
+++ src/scripts/experience/experience.js
@@ -33,11 +33,11 @@ export class Experience extends Component {
             <Col size="6">
               <p>
                 Glorious Codes is a Github organization where I publish all the
-                open source code that I have been creating on the latest years.
+                open-source code that I have been creating in the latest years.
               </p>
               <p>
                 I am very proud of having offered to the JavaScript community
-                a few open source libraries. I specially highlight <a
+                a few open-source libraries. I especially highlight <a
                 href="https://glorious.codes/demo" target="_blank">Glorious Demo
                 </a> that, beyond to be tweeted by <a
                 href="https://twitter.com/zenorocha/status/1057275408935194625"
@@ -64,9 +64,9 @@ export class Experience extends Component {
             <Col size="6">
               <h3>The Company</h3>
               <p>
-                Resultados Digitais was founded in Brazil in 2011 with the goal
-                of helping medium and small enterprises understand and leverage
-                the benefits of Digital Marketing, thereby achieving real and
+                Resultados Digitais was founded in Brazil in 2011 to help medium
+                and small enterprises understand and leverage the benefits of
+                Digital Marketing. RD Station allows them to achieve real and
                 long-lasting results for their businesses.
               </p>
               <h3>My Role</h3>
@@ -88,7 +88,7 @@ export class Experience extends Component {
             <Col size="6">
               <h3>The Company</h3>
               <p>
-                Bcredi is one of the most popular Fintechs in Brazil offering
+                Bcredi is one of the most popular Fintechs in Brazil, It offers
                 home equity loans with the best market rates. The amount of money
                 borrowed during its short life surpasses US$ 100 Million,
                 helping thousands of companies and people.
@@ -115,8 +115,8 @@ export class Experience extends Component {
               <p>
                 Created in 2012, Conta Azul is a SaaS that aims to help small
                 companies to run their business in Brazil. Conta Azul allows
-                business owners issue invoices, control their finances,
-                manage customers, receive payments and get great reports about
+                business owners to issue invoices, control their finances,
+                manage customers, receive payments, and get great reports about
                 their business. Conta Azul is used in all regions of Brazil
                 and serves dozens of thousands of customers.
               </p>
@@ -124,9 +124,9 @@ export class Experience extends Component {
               <p>
                 Among a lot of features developed in almost 5 years at Conta
                 Azul, I contributed with a few projects that I am very proud
-                of. In 2017, I had the opportunity to contribute to a
-                company’s internal product as the only front-end in the team,
-                being 100% responsible for designing all the web app's views
+                of. In 2017, I had the opportunity to collaborate with an
+                internal product as the only front-end in the team.
+                I was 100% responsible for designing all the web app views
                 too and, even more important, put into practice the first <b>
                 fully component-based UI project</b> made in the company.
               </p>
@@ -135,16 +135,16 @@ export class Experience extends Component {
                 began to disseminate the component-based architecture
                 inside the company, at guilds and front-end discussions, and
                 outside the company, writing an article entitled <b>Components
-                beyond reuse</b> published in portuguese on <a href="https://tableless.com.br/componentes-alem-do-reuso/" target="_blank">
-                Tableless</a> and in english on <a href="https://hackernoon.com/components-beyond-reuse-6cef6848a748" target="_blank">
+                beyond reuse</b> published in Portuguese on <a href="https://tableless.com.br/componentes-alem-do-reuso/" target="_blank">
+                Tableless</a> and in English on <a href="https://hackernoon.com/components-beyond-reuse-6cef6848a748" target="_blank">
                 Hackernoon</a>.
               </p>
               <p>
-                By the end of 2018, the company finally decided to replace the
-                MVC approach in favor of a component-based approach on its
-                main product. That was the moment which components became
+                By the end of 2018, the company finally started to replace the
+                Model-View-Controller approach for a Component-Based approach,
+                in its main product. That was the moment in which components became
                 first-class citizens in every tech discussion. A dedicated
-                team was created to handle <b>our internal component library</b> putting
+                team was created to handle <b>our internal component library</b>, putting
                 together the <b>most experienced front-end developers</b> and
                 UX designers.
               </p>
@@ -152,17 +152,17 @@ export class Experience extends Component {
                 Focusing on <b>high-quality</b> - <i>clean code and automated tests
                 </i> - and <b>productivity</b>, our components <a href="https://twitter.com/rcamargo/status/1152288995524644866" target="_blank">have
                 saved hours of programming time</a> and has contributed to
-                increase the consistency of user experience along the application.
+                increasing the consistency of user experience along with the application.
               </p>
               <p>
-                Beyond delivering great results to the product and to the
+                Beyond delivering some great results to the product and to the
                 company, I have <b>created our own documentation tool</b>. We
-                struggled to have a unified documentation containing our
-                legacy - <em>AngularJS</em> - and modern - <em>VueJS</em> components. <a
-                href="https://pitsby.com" target="_blank">Pitsby</a> solved our
-                problem once and for all. It’s open source and has even helped
-                to improve the experience of developing new components as well
-                as increasing the component library adoption by product teams.
+                struggled to have one documentation only, containing our legacy
+                and current components (AngularJS/Vue). <a href="https://pitsby.com" target="_blank">
+                Pitsby</a> solved our problem once and for all. It’s open-source
+                and has even helped to improve the experience of developing new
+                components as well as increasing the component library adoption
+                by product teams.
               </p>
             </Col>
           </Row>
@@ -186,11 +186,11 @@ export class Experience extends Component {
               </p>
               <h3>My Role</h3>
               <p>
-                In a brief period at Jobscore I had the opportunity to
-                collaborate to the website’s redesign project and its later
+                In a brief period at JobScore, I had the opportunity to
+                collaborate on its website redesign, and its later
                 improvements. Also, I introduced the AMD concept to the
-                JavaScript code making it modular, reusable and, more
-                important, fully testable. Jobscore was also the place where
+                JavaScript code, making it modular, reusable and, more
+                important, fully testable. JobScore was also the place where
                 I first touched Ruby on Rails.
               </p>
             </Col>
@@ -215,15 +215,15 @@ export class Experience extends Component {
               </p>
               <p>
                 I was responsible for creating the first company's style guide
-                intending to reduce the amount of CSS code written. In 2014
-                I helped to build the first reusable components
-                (at that time AngularJS forced us to call them "directives")
+                intending to reduce the amount of CSS code written. In 2014,
+                I helped to build the very first reusable components
+                (at that time, AngularJS forced us to call them <i>directives</i>)
                 written with AngularJS and fully covering them with unit tests.
               </p>
               <p>
-                During all that period I touched several front end technologies
+                During all that period I touched several front-end technologies,
                 including RequireJS (a library that implements the AMD pattern),
-                Grunt, AngularJS, Karma and Jasmine.
+                Grunt, AngularJS, Karma, and Jasmine.
               </p>
             </Col>
           </Row>
@@ -235,10 +235,10 @@ export class Experience extends Component {
             </Col>
             <Col size="6">
               <p>
-                From 2006 to 2013 I worked for a couple of advertising agencies.
-                A few of the works that I did can still be seen on my <a href="https://www.behance.net/rafaelcamargo" target="_blank">
+                From 2006 to 2013 I worked for few advertising agencies.
+                Some of the works that I did can still be seen on my <a href="https://www.behance.net/rafaelcamargo" target="_blank">
                 Behance</a> profile. I have been doing graphic design for a few hobby
-                projects too. The latest works can be seen on my <a href="https://dribbble.com/rcamargo" target="_blank">
+                projects too. The latest work can be seen on my <a href="https://dribbble.com/rcamargo" target="_blank">
                 Dribbble</a> profile.
               </p>
             </Col>
