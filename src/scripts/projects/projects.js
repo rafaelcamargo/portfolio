ff618f103a2c896978d32275c5e4918d4ba7f73a
Rafael Camargo <hello@rafaelcamargo.com>
2020-07-18 14:15:10 -0300

    feat: improve projects copy

--- src/scripts/projects/projects.js
+++ src/scripts/projects/projects.js
@@ -31,12 +31,12 @@ export class Projects extends Component {
                   image="logo-glorious-demo.svg"
                   title="Glorious Demo"
                   subtitle="2018"
-                  badgeText="Open Source"
+                  badgeText="Open-Source"
                   url="https://glorious.codes/demo"
                   ctaUrl="https://github.com/glorious-codes/glorious-demo"
                   ctaText="See The Code">
                   The easiest way of creating animations to show your code in
-                  action. Glorious Demo is open source, has been featured
+                  action. Glorious Demo is open-source, has been featured
                   on sites like <a href="https://news.ycombinator.com/item?id=18363668" target="_blank">
                   Hacker News</a> and <a href="https://www.reddit.com/r/javascript/comments/cavci6/the_easiest_way_of_creating_animations_to_show/" target="_blank">
                   Reddit</a>, and got the <b>Product of the Day</b> badge
@@ -47,13 +47,13 @@ export class Projects extends Component {
                   image="logo-pitsby.svg"
                   title="Pitsby"
                   subtitle="2019"
-                  badgeText="Open Source"
+                  badgeText="Open-Source"
                   url="https://pitsby.com"
                   ctaUrl="https://github.com/glorious-codes/glorious-pitsby"
                   ctaText="See The Code">
                   Pitsby is a super simple tool to document UI components in a
                   declarative way. Its main features are Lightning Setup, Wide
-                  Support, Playground, Analytics Integration and Customization.
+                  Support, Playground, Analytics Integration, and Customization.
                 </Project>
                 <Project
                   image="logo-nashios.svg"
@@ -69,30 +69,30 @@ export class Projects extends Component {
                   image="logo-woblast.svg"
                   title="Woblast"
                   subtitle="2019"
-                  badgeText="Open Source"
+                  badgeText="Open-Source"
                   url="https://woblast-app.web.app/"
                   ctaUrl="https://github.com/rafaelcamargo/woblast-web"
                   ctaText="See The Code">
                   A small application that fetches currency rates and stock
-                  exchange variations, and monitors them in real time. Built
-                  with React, HG Brasil Finance API and ChartJS. Logo and
+                  exchange variations, and monitors them in real-time. Built
+                  with React, HG Brasil Finance API, and ChartJS. Logo and
                   components designed by me.
                 </Project>
                 <Project
                   image="logo-weather-now.svg"
                   title="Weather Now"
                   subtitle="2018"
-                  badgeText="Open Source"
+                  badgeText="Open-Source"
                   url="https://weather-now-vue.web.app"
                   ctaUrl="https://github.com/rafaelcamargo/weather-now-vue"
                   ctaText="See The Code">
                   As a front-end developer at Conta Azul, I collaborated with
                   the hiring process of new developers designing a technical
-                  challenge for them. They should build a responsive small app
+                  assessment for them. They should build a tiny and responsive app
                   that fetched weather conditions from Open Weather Map API and
                   auto refreshed them after a couple minutes, using local
-                  cache in the meantime. That's my implementation in Vue to the
-                  challenge.
+                  cache in the meantime. This is my own implementation for
+                  that assessment using VueJS.
                 </Project>
                 <Project
                   image="logo-teleflik.svg"
@@ -100,7 +100,7 @@ export class Projects extends Component {
                   subtitle="2016"
                   url="https://play.google.com/store/apps/details?id=com.ionicframework.teleflik479683&hl=en_US">
                   Android app that helps you finding TV shows that match your
-                  interests on the following 24h. I did this to learn how to build a
+                  interests in the next 24h. I did this to learn how to build a
                   mobile app using Ionic and AngularJS. Logo and components
                   designed by me.
                 </Project>
