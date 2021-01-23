f2c17b00ee88ac6d697791f9b433d95cee5e68ba
Rafael Camargo <hello@rafaelcamargo.com>
2021-01-23 20:00:31 -0300

    feat: improve credits copy

--- src/scripts/base/components/credits/credits.js
+++ src/scripts/base/components/credits/credits.js
@@ -6,9 +6,9 @@ export class Credits extends Component {
     return (
       <div className="r-credits">
         <p>
-          This is an open-source website. If you wanna give a look at
-          it, <a href="https://github.com/rafaelcamargo/portfolio" target="_blank">
-          click here</a>.
+          This is an <a href="https://github.com/rafaelcamargo/portfolio" target="_blank">
+          open-source website</a>. Lots of UI components here are powered
+          by <a href="https://taslonic.com" target="_blank">Taslonic</a>.
         </p>
       </div>
     );
