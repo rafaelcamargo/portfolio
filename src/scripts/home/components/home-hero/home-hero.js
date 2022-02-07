aee7000cffffcdcf56bd8ee691f6b5870754b101
Rafael Camargo <hello@rafaelcamargo.com>
2022-02-07 14:33:54 -0300

    chore: improve home hero copy

--- src/scripts/home/components/home-hero/home-hero.js
+++ src/scripts/home/components/home-hero/home-hero.js
@@ -8,7 +8,7 @@ export const HomeHero = () => {
     <div className="r-home-hero">
       <ProjectFeaturedList />
       <h1>
-        All those projects were fully covered with automated tests.<br />
+        All those side projects were fully covered with automated tests.<br />
         My name is Rafael Camargo. I am a programmer, designer, and believe it's quite possible to make very fast something that works very well.
       </h1>
       <CtaLink to="/projects" text="See all projects" />
