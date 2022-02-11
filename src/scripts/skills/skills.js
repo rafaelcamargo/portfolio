45fcc71bc3282a3b14a90330c9d76cc1f9ad3d50
Rafael Camargo <hello@rafaelcamargo.com>
2022-02-11 16:28:38 -0300

    feat: redesign skills page

--- src/scripts/skills/skills.js
+++ src/scripts/skills/skills.js
@@ -1,70 +1,51 @@
-import React, { Component } from 'react';
-import { Col } from '@scripts/base/components/col/col';
+import React from 'react';
 import { Hero } from '@scripts/base/components/hero/hero';
-import { Row } from '@scripts/base/components/row/row';
 import { Section } from '@scripts/base/components/section/section';
 import { Viewport } from '@scripts/base/components/viewport/viewport';
 
 const meta = {
   title: 'Skills',
-  description: 'After working for over seven years as a graphic designer, I started on 2013 my career as a programmer. Along this rich period, I got fluent with the latest JavaScript specifications, several front-end technologies and got deep in concepts that have shaped the way I write software.',
-  keywords: 'gulp, webpack, spa, angularjs, vue, react, karma, jasmine, jest, postcss, sass, less, stylus, nodejs, express, mongodb, ruby, rails, postgres, docker'
+  description: 'Before becoming a programmer, I worked as a graphic designer for over seven years. I created logotypes, packages, websites, advertising and animations. My biggest skill as a programmer is not to be a programmer only',
+  keywords: 'design, programming, clean code, clean interfaces, SOLID, Gestalt'
 };
 
-export class Skills extends Component {
-  render() {
-    return (
-      <Viewport
-        title={ meta.title }
-        description={ meta.description }
-        keywords={ meta.keywords }>
-        <Hero title="Skills" size="small" />
-        <Section>
-          <Row >
-            <Col size="6">
-              <p>
-                After working for over seven years as a graphic designer, I <b>
-                started in 2013</b> my career as a programmer at one of the <b>most
-                popular startups in Brazil</b>, where I ended up leading
-                the <b>front-end</b> efforts in the <b> Design System</b> team.
-              </p>
-              <p>
-                During that exciting period, I got fluent in
-                the <b>latest JavaScript specifications</b>, several front-end
-                technologies, and went deep in concepts that have shaped the
-                way I write software. Among these concepts are <b>Clean Code
-                </b>, <b>SOLID</b>, the <b>three laws of TDD</b>, and this <a
-                  href="https://blog.cleancoder.com/uncle-bob/2011/01/17/software-craftsmanship-is-about.html"
-                  rel="noreferrer"
-                  target="_blank">
-                  blog post
-                </a> on <b>Software Craftsmanship</b> written by Robert Martin.
-              </p>
-            </Col>
-            <Col size="6">
-              <p>
-                I am very experienced with bundlers and task runners like <b>Gulp
-                </b> and <b>Webpack</b>. Long experience building <b>Single Page
-                Applications</b> since the old <b>AngularJS</b> until the
-                modern <b>Vue</b> and <b>React</b>. I am very familiar with
-                unit test tools like <b>Karma</b>, <b>Jasmine</b>, and <b>Jest</b>.
-                I’m very familiar with a wide range of CSS preprocessors
-                like <b>Sass</b>, <b>Less</b>, <b>PostCSS</b> and, mainly, <b>Stylus</b>.
-              </p>
-              <p>
-                Topics other than front-end on which I have a good
-                understanding include Continuous Integration tools like <b>
-                Travis</b> and <b>CircleCI</b>. <b>Firebase</b>, <b>Netlify
-                </b> and <b>AWS S3</b>, test coverage tools like <b>Coveralls
-                </b> and <b>Codecov</b>, <b>HTTP</b> and <b>REST</b>. Also, I
-                have a basic understanding of the following back-end
-                technologies: <b>NodeJS</b>, <b>Express</b>, <b>MongoDB</b>, <b>
-                Ruby</b>, <b>Rails</b>, <b>Postgres</b> and <b>Docker</b>.
-              </p>
-            </Col>
-          </Row>
-        </Section>
-      </Viewport>
-    );
-  }
+const CONTAINER_SIZE = 'sm';
+
+export const Skills = () => {
+  return (
+    <Viewport
+      title={meta.title}
+      description={meta.description}
+      keywords={meta.keywords}
+      topbarContainerSize={CONTAINER_SIZE}
+      hideMenu>
+      <Hero title="Skills" size="small" containerSize={CONTAINER_SIZE} />
+      <Section containerSize={CONTAINER_SIZE} size="small">
+        <p>
+          One of the greatest Brazilian songwriters is not just a songwriter.
+          She also composes, sings and plays. Marisa Monte masters the creative
+          process from idea to production. Her work is astonishing because
+          she knows profoundly every detail of the whole.
+        </p>
+        <p>
+          Splitting user interface development between design and programming
+          roles is a mistake. Isolated in their specializations, both are
+          incapable of understanding the whole. A messy interface makes
+          excellent code useless. A messy code makes impressive interfaces
+          buggy.
+        </p>
+        <p>
+          Before becoming a programmer, I worked as a graphic designer for
+          over seven years. I created logotypes, packages, websites,
+          advertising and animations. My biggest skill as a programmer is
+          not to be a programmer only.
+        </p>
+        <p>
+          Clean code is as important as clean interfaces. SOLID principles
+          are as valuable as Gestalt principles. I code considering design.
+          I design considering code. I know profoundly every detail of the whole.
+        </p>
+      </Section>
+    </Viewport>
+  )
 }
