ea9b30463c501b24ede5f6c0b90c9d0973fc581e
Rafael Camargo <hello@rafaelcamargo.com>
2022-02-12 19:24:46 -0300

    chore: redesign influences page

--- src/scripts/influences/influences.js
+++ src/scripts/influences/influences.js
@@ -1,33 +1,47 @@
-import React, { Component } from 'react';
-import { Col } from '@scripts/base/components/col/col';
+import React from 'react';
+import { Row, Col } from '@glorious/taslonic/react';
 import { Hero } from '@scripts/base/components/hero/hero';
-import { Row } from '@scripts/base/components/row/row';
 import { Section } from '@scripts/base/components/section/section';
 import { Viewport } from '@scripts/base/components/viewport/viewport';
 import { InfluencesList } from '@scripts/influences/components/influences-list/influences-list';
 
 const meta = {
   title: 'Influences',
-  description: 'Once in a while, friends and strangers ask me about what are the contents that have influenced me most. I used to answer them individually by email, direct messages, and occasional chats. But my memory is always like a hard disk requiring more space, so I decided to put all the great things that have shaped the way I see the world in the following list. I hope some of these references can surprise you the same way they have surprised me.',
+  description: 'Once in a while, I get asked what has influenced me most. Here is a list of things that have improved my life.',
   keywords: 'influences, books, videos, posts'
 };
 
-export class Influences extends Component {
-  render() {
-    return (
-      <Viewport
-        title={ meta.title }
-        description={ meta.description }
-        keywords={ meta.keywords }>
-        <Hero title="Influences" size="small" sectionTheme="primary" />
-        <Section>
-          <Row>
-            <Col size="12">
-              <InfluencesList />
-            </Col>
-          </Row>
-        </Section>
-      </Viewport>
-    );
-  }
+const CONTAINER_SIZE = 'sm';
+
+export const Influences = () => {
+  return (
+    <Viewport
+      title={ meta.title }
+      description={ meta.description }
+      keywords={ meta.keywords }
+      topbarContainerSize={CONTAINER_SIZE}
+      hideMenu>
+      <Hero title="Influences" size="small" containerSize={CONTAINER_SIZE} />
+      <Section size="small" containerSize={CONTAINER_SIZE}>
+        <Row>
+          <Col xs="12">
+            <p>
+              Once in a while, I get asked what has influenced me most. I used
+              to answer it individually by email, direct messages, and occasional
+              chats. But my memory is a hard disk always requiring more space,
+              so I ended up forgetting something. Then, I decided to put all
+              the great things that have shaped the way I see the world in the
+              following list. May these references surprise you as much as
+              they have surprised me.
+            </p>
+          </Col>
+        </Row>
+        <Row offsetXs="4">
+          <Col xs="12">
+            <InfluencesList />
+          </Col>
+        </Row>
+      </Section>
+    </Viewport>
+  );
 }
--- src/scripts/influences/influences.test.js
+++ src/scripts/influences/influences.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { Hero } from '@scripts/base/components/hero/hero';
 import { Viewport } from '@scripts/base/components/viewport/viewport';
+import { InfluencesList } from '@scripts/influences/components/influences-list/influences-list';
 import { Influences } from '@scripts/influences/influences';
 
 describe('Influences', () => {
@@ -20,4 +21,9 @@ describe('Influences', () => {
     expect(wrapper.find(Hero).prop('title')).toEqual('Influences');
     expect(wrapper.find(Hero).prop('size')).toEqual('small');
   });
+
+  it('should contain influences list', () => {
+    const wrapper = mount();
+    expect(wrapper.find(InfluencesList).length).toEqual(1);
+  })
 });
