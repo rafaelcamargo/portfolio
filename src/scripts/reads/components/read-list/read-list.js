79c4935848e22bd0c502a7a5303469c6430b1d8b
Rafael Camargo <hello@rafaelcamargo.com>
2022-02-16 23:46:25 -0300

    chore: create read list

---
+++ src/scripts/reads/components/read-list/read-list.js
@@ -0,0 +1,48 @@
+import '@styles/read-list.styl';
+import React from 'react';
+import readService from '@scripts/reads/services/read/read';
+import dateService from '@scripts/base/services/date/date';
+
+export const ReadList = () => {
+  const reads = groupReadsByMonth(readService.get());
+  return (
+    <ul className="r-read-list">
+      {Object.keys(reads).map(monthString => (
+        <li key={monthString} className="r-read-list-group" data-read-group>
+          <h2>{buildGroupTitle(monthString)}</h2>
+          <ul>
+            {reads[monthString].map(({ title, author }, index) => (
+              <li key={index}>
+                <h3>{title}</h3>
+                <p>{author}</p>
+              </li>
+            ))}
+          </ul>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+function groupReadsByMonth(reads){
+  const groups = {};
+  Object.keys(reads).forEach(bookId => {
+    const book = reads[bookId];
+    if(book.read_on) getGroupByKey(groups, buildBookGroupKey(book)).push(book);
+  });
+  return groups;
+}
+
+function buildBookGroupKey(book){
+  const [year, month] = book.read_on.split('-');
+  return `${year}-${month}`
+}
+
+function getGroupByKey(groups, key){
+  if(!groups[key]) groups[key] = [];
+  return groups[key];
+}
+
+function buildGroupTitle(monthString){
+  return dateService.formatMonthDescriptively(monthString);
+}
---
+++ src/scripts/reads/components/read-list/read-list.test.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import readsMock from '@scripts/reads/mocks/reads.json';
+import readService from '@scripts/reads/services/read/read';
+import { ReadList } from '@scripts/reads/components/read-list/read-list';
+
+describe('Read List', () => {
+  function mount(props = {}){
+    return shallow(<ReadList />);
+  }
+
+  beforeEach(() => {
+    readService.get = jest.fn(() => readsMock);
+  });
+
+  it('should group reading by month', () => {
+    const wrapper = mount();
+    const groups = wrapper.find('[data-read-group]');
+    expect(groups).toHaveLength(3)
+    expect(groups.at(0).find('h2').text()).toEqual('2022 January');
+    expect(groups.at(2).find('h2').text()).toEqual('2021 November');
+  });
+
+  it('should list reads for each month', () => {
+    const wrapper = mount();
+    const groups = wrapper.find('[data-read-group]');
+    expect(groups.at(0).find('h3')).toHaveLength(3)
+    expect(groups.at(0).find('h3').at(0).text()).toEqual('Out of the Wreckage');
+    expect(groups.at(0).find('p').at(0).text()).toEqual('George Monboit');
+    expect(groups.at(0).find('h3').at(2).text()).toEqual('As intermitências da morte');
+    expect(groups.at(0).find('p').at(2).text()).toEqual('José Saramago');
+    expect(groups.at(2).find('h3')).toHaveLength(6)
+    expect(groups.at(2).find('h3').at(0).text()).toEqual('How to Live');
+    expect(groups.at(2).find('p').at(0).text()).toEqual('Derek Sivers');
+    expect(groups.at(2).find('h3').at(5).text()).toEqual('O homem que roubava horas');
+    expect(groups.at(2).find('p').at(5).text()).toEqual('Daniel Munduruku');
+  });
+});
