5bf1217997589216a4e36b966b193e780c8a803f
Rafael Camargo <hello@rafaelcamargo.com>
2022-02-28 22:54:59 -0300

    chore: add link to tools on skills view

--- src/scripts/skills/skills.js
+++ src/scripts/skills/skills.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Row, Col } from '@glorious/taslonic/react';
+import { CtaLink } from '@scripts/base/components/cta-link/cta-link';
 import { Hero } from '@scripts/base/components/hero/hero';
 import { Section } from '@scripts/base/components/section/section';
 import { Viewport } from '@scripts/base/components/viewport/viewport';
@@ -21,30 +23,39 @@ export const Skills = () => {
       hideMenu>
       <Hero title="Skills" size="small" containerSize={CONTAINER_SIZE} />
       <Section containerSize={CONTAINER_SIZE} size="small">
-        <p>
-          One of the greatest Brazilian songwriters is not just a songwriter.
-          She also composes, sings and plays. Marisa Monte masters the creative
-          process from idea to production. Her work is astonishing because
-          she knows profoundly every detail of the whole.
-        </p>
-        <p>
-          Splitting user interface development between design and programming
-          roles is a mistake. Isolated in their specializations, both are
-          incapable of understanding the whole. A messy interface makes
-          excellent code useless. A messy code makes impressive interfaces
-          buggy.
-        </p>
-        <p>
-          Before becoming a programmer, I worked as a graphic designer for
-          over seven years. I created logotypes, packages, websites,
-          advertising and animations. My biggest skill as a programmer is
-          not to be a programmer only.
-        </p>
-        <p>
-          Clean code is as important as clean interfaces. SOLID principles
-          are as valuable as Gestalt principles. I code considering design.
-          I design considering code. I know profoundly every detail of the whole.
-        </p>
+        <Row>
+          <Col xs="12">
+            <p>
+              One of the greatest Brazilian songwriters is not just a songwriter.
+              She also composes, sings and plays. Marisa Monte masters the creative
+              process from idea to production. Her work is astonishing because
+              she knows profoundly every detail of the whole.
+            </p>
+            <p>
+              Splitting user interface development between design and programming
+              roles is a mistake. Isolated in their specializations, both are
+              incapable of understanding the whole. A messy interface makes
+              excellent code useless. A messy code makes impressive interfaces
+              buggy.
+            </p>
+            <p>
+              Before becoming a programmer, I worked as a graphic designer for
+              over seven years. I created logotypes, packages, websites,
+              advertising and animations. My biggest skill as a programmer is
+              not to be a programmer only.
+            </p>
+            <p>
+              Clean code is as important as clean interfaces. SOLID principles
+              are as valuable as Gestalt principles. I code considering design.
+              I design considering code. I know profoundly every detail of the whole.
+            </p>
+          </Col>
+        </Row>
+        <Row offsetXs="4">
+          <Col xs="12">
+            <CtaLink to="/tools" text="See my toolbox" />
+          </Col>
+        </Row>
       </Section>
     </Viewport>
   )
