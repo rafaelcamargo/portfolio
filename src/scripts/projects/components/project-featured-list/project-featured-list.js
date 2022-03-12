b1e210478e04e35d9d7ec76c4c4166e04f048ccb
Rafael Camargo <hello@rafaelcamargo.com>
2022-03-12 15:35:10 -0300

    chore: improve project featured list
    wrap headings in tags

--- src/scripts/projects/components/project-featured-list/project-featured-list.js
+++ src/scripts/projects/components/project-featured-list/project-featured-list.js
@@ -1,5 +1,6 @@
 import '@styles/project-featured-list.styl';
 import React from 'react';
+import { Tag } from '@glorious/taslonic/react';
 import projects from '@scripts/projects/data/featured-projects.json';
 
 export const ProjectFeaturedList = () => {
@@ -9,7 +10,9 @@ export const ProjectFeaturedList = () => {
         <ul className="r-project-featured-list">
           {projects.map(({ description, days_invested }, index) => (
             <li key={index}>
-              <h2>In {days_invested} days</h2>
+              <Tag>
+                <h2>In {days_invested} days</h2>
+              </Tag>
               <p>{description}</p>
             </li>
           ))}
