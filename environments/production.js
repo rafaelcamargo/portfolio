454704fb1b570db7904ceec73ac444e96a11a724
Rafael Camargo <hello@rafaelcamargo.com>
2022-11-11 12:50:40 -0300

    chore: change vervet base url (to vercel)

--- environments/production.js
+++ environments/production.js
@@ -9,7 +9,7 @@ module.exports = {
   },
   NEWSLETTER_ID: 'rafaelcamargo',
   VERVET: {
-    BASE_URL: 'https://vervet-production.up.railway.app',
+    BASE_URL: 'https://vervet-api.vercel.app',
     PROJECTS: {
       MAILER: {
         ID: 'be51fc60-6e82-41cd-8e5a-091cc1fa6380',
